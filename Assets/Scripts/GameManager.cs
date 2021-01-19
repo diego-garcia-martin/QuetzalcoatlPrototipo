6b1276e02d7878cb66b00e2a44b6a53cf1dd0159
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-01-18 20:16:55 -0600

    Commit Inicial del juego
    El primer protoripo del juego

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float tileSpeed;
+    public GameObject tile;
+    public List<GameObject> l_tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        l_tile.Add(Instantiate(tile, new Vector3(0, 0, 0), Quaternion.identity));
+
+        for(int i = 1; i <= 10; i++)
+        {
+            l_tile.Add(Instantiate(tile, new Vector3(Random.Range(-8, 8), 3*i, 0), Quaternion.identity));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < l_tile.Count; i++)
+        {
+            l_tile[i].transform.position = l_tile[i].transform.position + new Vector3(0, -1f, 0) * Time.deltaTime * tileSpeed;
+            if(l_tile[i].transform.position.y < -6)
+            {
+               l_tile[i].transform.position = new Vector3(Random.Range(-8, 8), 27, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/sc_player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D r2d;
+    private Transform tr;
+    public float jumpForce;
+    public float moveSpeed;
+    private bool jumping;
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirx = 0;
+        if(Input.touchCount == 0)
+        {
+            jumping = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && jumping == false))
+        {
+            r2d.AddForce(new Vector2(0, jumpForce));
+            jumping = true;
+        }
+
+        dirx = Input.acceleration.x * moveSpeed;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            dirx = -moveSpeed;
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            dirx = moveSpeed;
+        }
+
+        r2d.velocity = new Vector2(dirx, r2d.velocity.y);
+
+        if(tr.position.y < -5) tr.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
