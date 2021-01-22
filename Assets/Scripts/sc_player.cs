43a13457f3056c10528762792a6c9faf1813802c
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-01-21 21:24:33 -0600

    Arreglos al personaje
    Al menos hasta que le agreguemos vida u otra funcionalidad, lo que esta del personaje ya funciona

--- Assets/Scripts/sc_player.cs
+++ Assets/Scripts/sc_player.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class sc_player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const string IDLE = "Anim_Quetzalcoatl_idle";
+    private const string RUN = "Anim_Quetzalcoatl_run";
+    private const string JUMP = "Anim_Quetzalcoatl_jump";
+
     private Rigidbody2D r2d;
     private Transform tr;
-    private int jumping;
-    private string currentAnim;
     private Animator animator;
+    private string currentAnim;
+    private int jumping;
+    private bool grounded;
+    private bool touchEnable;
     public float jumpForce;
     public float moveSpeed;
     public int maxJumps;
     public bool debugMode;
 
-    private const string IDLE = "Anim_Quetzalcoatl_idle";
-    private const string RUN = "Anim_Quetzalcoatl_run";
-    private const string JUMP = "Anim_Quetzalcoatl_jump";
-
     void Start()
     {
+        Input.multiTouchEnabled = false;
+        touchEnable = true;
         currentAnim = "";
         r2d = GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
@@ -34,7 +37,7 @@ public class sc_player : MonoBehaviour
     {
         //Esto es para pruebas, si se sale de la pantalla el mono que vuelva a entrar y mostrar las velocidades y valores de interes
         if(debugMode){
-            debugInfo();
+            //debugInfo();
             if(tr.position.y < -5)
             {
                 tr.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.identity);
@@ -48,35 +51,47 @@ public class sc_player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        float dify = tr.position.y - collision.transform.position.y;
+        if(collision.gameObject.tag == "Ground" && dify > 0)
         {
             jumping = 0;
+            grounded = true;
         }
     }
 
     private void updateAnimations()
     {
-        if(r2d.velocity.x != 0)
+        if(grounded || r2d.velocity.y <= 0)
         {
-            changeAnimation(RUN);
-            if(r2d.velocity.x < 0) tr.localScale = new Vector3(1, 1, 1);
-            else tr.localScale = new Vector3(-1, 1, 1);
+            if(r2d.velocity.x != 0)
+            {
+                changeAnimation(RUN);
+                if(r2d.velocity.x < 0) tr.localScale = new Vector3(1, 1, 1);
+                else tr.localScale = new Vector3(-1, 1, 1);
+            }
+            else changeAnimation(IDLE);
         }
-        else changeAnimation(IDLE);
     }
 
     private void updateMovement()
     {
         float dirx = 0;
+
+        if(Input.touchCount <= 0)
+        {
+            touchEnable = true;
+        }
         
-        if((Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0) && jumping < maxJumps)
+        if((Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && touchEnable)) && jumping < maxJumps)
         {
             r2d.velocity = new Vector2( r2d.velocity.x, jumpForce);
             jumping++;
             changeAnimation(JUMP);
+            grounded = false;
+            touchEnable = false;
         }
 
-        dirx = Input.acceleration.x * moveSpeed;
+        dirx = Input.acceleration.x * moveSpeed * 2.5f;
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -92,6 +107,10 @@ public class sc_player : MonoBehaviour
 
     private void changeAnimation(string newAnim)
     {
+        if(currentAnim == JUMP && newAnim == JUMP)
+        {
+            animator.Play(newAnim, -1, 0f);
+        }
         if(currentAnim == newAnim) return;
         animator.Play(newAnim);
         currentAnim = newAnim;
