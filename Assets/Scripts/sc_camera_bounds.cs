d69a890f402d44e1ee1f50b2737783e1cd14bd31
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-01-22 12:49:39 -0600

    Arreglado script para limitar movimiento a la pantalla
    El script limita el movimiento a los limites de la pantalla, se puede desactivar la opcion de limitar el movimiento en Y para los obetos que si puedan salir hacia arriba o abajo como enemigos

--- Assets/Scripts/sc_camera_bounds.cs
+++ Assets/Scripts/sc_camera_bounds.cs
@@ -9,13 +9,14 @@ public class sc_camera_bounds : MonoBehaviour
     private Vector3 limitsMax;
     private Transform tr;
     private Vector2 spriteSize;
+    public bool limitYmovement;
         // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
         cam = Camera.main;
         limitsMin = cam.ScreenToWorldPoint(new Vector3(0, 0, tr.position.z));
-        limitsMin = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, tr.position.z));
+        limitsMax = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, tr.position.z));
         spriteSize.x = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
         spriteSize.y = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
         
@@ -25,8 +26,8 @@ public class sc_camera_bounds : MonoBehaviour
     void LateUpdate()
     {
         Vector3 viewPos = tr.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, limitsMin.x + spriteSize.x, limitsMax.x -spriteSize.x);
-        viewPos.y = Mathf.Clamp(viewPos.y, limitsMin.y + spriteSize.y, limitsMax.y -spriteSize.y);
+        viewPos.x = Mathf.Clamp(viewPos.x, limitsMin.x + spriteSize.x, limitsMax.x - spriteSize.x);
+        if (limitYmovement) viewPos.y = Mathf.Clamp(viewPos.y, limitsMin.y, limitsMax.y);
         tr.position = viewPos;
     }
 }
