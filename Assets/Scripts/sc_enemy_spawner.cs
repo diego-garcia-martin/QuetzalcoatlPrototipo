b6cc096670a53dd15aed7ae395dc303e43598a76
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-02-01 21:53:51 -0600

    Creacion de Enemy spawner
    Ya funciona mejor en el nivel el pichoncin y ya hay un objeto de enemy spawner que los pone en el mapa

---
+++ Assets/Scripts/sc_enemy_spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_enemy_spawner : MonoBehaviour
+{
+    //Variables para el timer de spawn de enemigos
+    public float spawnTimer;
+    private float elapsedTime;
+    public GameObject enemy1;
+    private List<GameObject> l_enemies;
+    private const float MIN_POS_IN_Y = -6;
+    private const float MAX_POS_IN_Y = 6;
+    private const float MIN_POS_IN_X = -8;
+    private const float MAX_POS_IN_X = 8;
+    public int MAX_ENEMY_COUNT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        l_enemies = new List<GameObject>();
+        elapsedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > spawnTimer && l_enemies.Count < MAX_ENEMY_COUNT)
+        {
+            elapsedTime = 0;
+            l_enemies.Add(Instantiate(enemy1, new Vector3(Random.Range(MIN_POS_IN_X, MAX_POS_IN_X), MAX_POS_IN_Y, 0), Quaternion.identity));
+        }
+
+        for(int index = 0; index < l_enemies.Count; index ++)
+        {
+            if (l_enemies[index].transform.position.y < MIN_POS_IN_Y)
+            {
+                GameObject.Destroy(l_enemies[index]);
+                l_enemies.RemoveAt(index);
+            }
+        }
+    }
+}
--- Assets/Scripts/sc_pichoncin.cs
+++ Assets/Scripts/sc_pichoncin.cs
@@ -8,8 +8,14 @@ public class sc_pichoncin : MonoBehaviour
     private Animator animator;
     private Rigidbody2D r2d;
     private Transform tr;
+    private BoxCollider2D col;
+    private Camera cam;
+    private Vector3 limitsMin;
+    private Vector3 limitsMax;
     //Variables de control para ver que le pasa al pichoncin
     private bool isGrounded;
+    private bool isFlying;
+    public int segundosAccion;
     //Las variables que tienen que ver con las animaciones
     private string currentAnim;
     private const string IDLE = "Anim_Pichoncin_idle";
@@ -22,6 +28,10 @@ public class sc_pichoncin : MonoBehaviour
         animator = GetComponent<Animator>();
         r2d = GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
+        col = GetComponent<BoxCollider2D>();
+        cam = Camera.main;
+        limitsMin = cam.ScreenToWorldPoint(new Vector3(0, 0, tr.position.z));
+        limitsMax = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, tr.position.z));
         currentAnim = "";
         changeAnimation(FLY);
         isGrounded = false;
@@ -31,18 +41,64 @@ public class sc_pichoncin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!isGrounded) changeAnimation(FLY);
-        if(r2d.velocity.x != 0)
+        if (!isGrounded)
+        {
+            changeAnimation(FLY);
+            if (isFlying) flying();
+            if (r2d.velocity.y < -10)
             {
-                if(r2d.velocity.x < 0) tr.localScale = new Vector3(-1, 1, 1);
-                else tr.localScale = new Vector3(1, 1, 1);
+                r2d.AddForce(new Vector2(Random.Range(-300, 300), 300));
+                isFlying = true;
+                isGrounded = false;
             }
-        
+        }
+        if (r2d.velocity.x != 0)
+        {
+            if (r2d.velocity.x < 0) tr.localScale = new Vector3(-1, 1, 1);
+            else tr.localScale = new Vector3(1, 1, 1);
+        }
+        if (r2d.velocity.y > 0)
+        {
+            col.enabled = false;
+        }
+        if (r2d.velocity.y <= 0)
+        {
+            col.enabled = true;
+        }
+
+        if (tr.position.x >= limitsMax.x - 1)
+        {
+            r2d.velocity = new Vector3(-2, r2d.velocity.x, 0);
+        }
+        else if (tr.position.x <= limitsMin.x + 1)
+        {
+            r2d.velocity = new Vector3(2, r2d.velocity.x, 0);
+        }
+
+        if (r2d.velocity.y < -10 && isGrounded)
+        {
+            r2d.AddForce(new Vector2(Random.Range(-300, 300), 300));
+            isFlying = true;
+            isGrounded = false;
+        }
+        else if (r2d.velocity.y < -3)
+        {
+            isGrounded = false;
+        }
+
+    }
+
+    private void flying()
+    {
+        if (r2d.velocity.y <= 0)
+        {
+            r2d.velocity = new Vector3(r2d.velocity.x, 0, 0);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
             r2d.velocity = new Vector2(0, 0);
@@ -52,24 +108,25 @@ public class sc_pichoncin : MonoBehaviour
 
     private void changeAnimation(string newAnim)
     {
-        if(currentAnim == newAnim) return;
+        if (currentAnim == newAnim) return;
         animator.Play(newAnim);
         currentAnim = newAnim;
     }
 
     IEnumerator waiter()
     {
-        for(;;)
+        for (; ; )
         {
-            if(isGrounded)
+            if (isGrounded && !isFlying)
             {
-                if(Random.Range(0, 10) < 5)
+                if (Random.Range(0, 10) < 5)
                 {
                     changeAnimation(EAT);
                 }
-                else if(Random.Range(0, 10) < 6)
+                else if (Random.Range(0, 10) < 6)
                 {
-                    r2d.AddForce(new Vector2(Random.Range(-300, 300), 300));
+                    r2d.AddForce(new Vector2(Random.Range(-300, 300), 600));
+                    isFlying = true;
                     isGrounded = false;
                 }
                 else
@@ -77,7 +134,18 @@ public class sc_pichoncin : MonoBehaviour
                     changeAnimation(IDLE);
                 }
             }
-            yield return new WaitForSeconds(2); 
+            else if (isFlying)
+            {
+                if (Random.Range(0, 10) < 7)
+                {
+                    r2d.AddForce(new Vector2(Random.Range(-300, 300), 0));
+                }
+                else
+                {
+                    isFlying = false;
+                }
+            }
+            yield return new WaitForSeconds(segundosAccion);
         }
     }
 }
