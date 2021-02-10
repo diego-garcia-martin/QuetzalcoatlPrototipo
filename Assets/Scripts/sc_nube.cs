9a04b5b052771558a20c2baf7cf5f0b1caf72d69
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-02-09 21:57:54 -0600

    Prefab de nube y su script
    Falta ponerle el sprite que es

---
+++ Assets/Scripts/sc_nube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_nube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float vidaNube;
+    private float timer;
+    private SpriteRenderer sr;
+    private BoxCollider2D col;
+    void Start()
+    {
+        timer = vidaNube;
+        sr = GetComponent<SpriteRenderer>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer - Time.deltaTime;
+
+        float alpha = timer / vidaNube;
+
+        sr.color = new Color(1f, 1f, 1f, alpha);
+
+        if (alpha < 0.3f)
+        {
+            col.enabled = false;
+        } 
+
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
