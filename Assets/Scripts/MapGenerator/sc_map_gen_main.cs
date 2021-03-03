ae18e1c222f8c9a569506f92860496cc0401ed92
alexzc117 <alexzc117@gmail.com>
2021-03-02 22:46:12 -0600

    New Objects Spawner Motor
    Pos lo que dice bro, motor nuevo, porque ALGUIEN se le ocurrio la MARAVILLOSA idea de lo hiciera, va a quedar mamalon

---
+++ Assets/Scripts/MapGenerator/sc_map_gen_main.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_map_gen_main : MonoBehaviour
+{
+    //Variables Generales
+    public float TileSpeed;
+    public int NivelDeDificultad;
+    //Probabilidad de aparicion de escenarios, deben sumar 100 en total y no ser 0
+    public int ProbabilidadEscenarioNormal;
+    public int ProbabilidadEscenarioCongelado;
+    public int ProbabilidadEscenarioFalso;
+    public int ProbabilidadEscenarioDeNubes;
+    //Probabilidad de aparicion de Tipos de Plataformas en cada escenario, se modifican dependiendo del escenario, deben sumar 100 y no ser 0
+    public int ProbabilidadPlataformaNormal;
+    public int ProbabilidadPlataformaRoca;
+    public int ProbabilidadPlataformaCongelada;
+    public int ProbabilidadPlataformaFalsa;
+    public int ProbabilidadPlataformaNube;
+    //Probabilidad de aparicion de Tipos de Trampas en los escenarios, no deben ser 0, la probabilidad restante a 100 se asiganara a posibilidad de no aparecer nada
+    public int ProbabilidadDePicos;
+    public int ProbabilidadDeExplosion;
+    //Probabilidad de aparicion de miscelanios en el mapa, no debe ser 0, la probabilidad restante a 100 se asignara a posibililidad de no aparecer nada
+    public int ProbabilidadDeMiscelaneos;
+
+    //Entrada de Objetos
+    public GameObject TileNormal;
+    public GameObject TileRock;
+    public GameObject TileFrezee;
+    public GameObject TileFake;
+    public GameObject TileCloud;
+    public GameObject TrampSpikes;
+    public GameObject TrampExplosion;
+    public GameObject Miscelaneos; //Discusion con equipo para revisar cuales existiran y crear el diccionario correspondiente
+
+    //Diccionario de Objectos
+    private enum eDiccionaryTypeObject
+    {
+        TypeObjectStart,
+        TypeObjectTiles = TypeObjectStart,
+        TypeObjectTramp,
+        TypeObjectMiscelaneo,
+        TypeObjectEnd
+    }
+
+    //Indice de probabilidad de escenarios
+    private enum eTypeEscenarios
+    {
+        TypeEscenariosStart,
+        TypeNormal = TypeEscenariosStart,
+        TypeCongelado,
+        TypeFalso,
+        TypeNubes,
+        TypeEscenariosEnd
+    }
+
+    private struct StructDataObject
+    {
+        public GameObject Object;
+        public int ProbabilityMinor;
+        public int ProbabilityMayor;
+    }
+
+    private struct StructEscenarioData 
+    {
+        public int ProbabilityMinor;
+        public int ProbabilityMayor;
+    }
+
+    private Dictionary<eDiccionaryTypeObject, List<StructDataObject>> DictionaryOfGameObjects;
+
+    private Dictionary<eTypeEscenarios, StructEscenarioData> ProbablyEscenarios;
+
+    private List<StructDataObject> initTilesObjects() 
+    {
+        List<StructDataObject> listRet = new List<StructDataObject>();
+        listRet.Add(new StructDataObject() { Object = TileNormal, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadPlataformaNormal });
+        listRet.Add(new StructDataObject() { Object = TileRock, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadPlataformaRoca });
+        listRet.Add(new StructDataObject() { Object = TileFrezee, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadPlataformaCongelada });
+        listRet.Add(new StructDataObject() { Object = TileFake, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadPlataformaFalsa });
+        listRet.Add(new StructDataObject() { Object = TileCloud, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadPlataformaNube });
+        return listRet;
+    }
+
+    private List<StructDataObject> initTrampsObjects() 
+    {
+        List<StructDataObject> listRet = new List<StructDataObject>();
+        listRet.Add(new StructDataObject() { Object = TrampSpikes, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadDePicos });
+        listRet.Add(new StructDataObject() { Object = TrampExplosion, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadDeExplosion });
+        return listRet;
+    }
+
+    private List<StructDataObject> initMicelaneosObjects()
+    {
+        List<StructDataObject> listRet = new List<StructDataObject>();
+        listRet.Add(new StructDataObject() { Object = Miscelaneos, ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadDeMiscelaneos });
+        return listRet;
+    }
+
+    private void initDictionaryOfGameObjects() 
+    {
+        //Inicia y agrega listas de objetos de juego
+        DictionaryOfGameObjects = new Dictionary<eDiccionaryTypeObject, List<StructDataObject>>();
+        DictionaryOfGameObjects.Add(eDiccionaryTypeObject.TypeObjectTiles, initTilesObjects());
+        DictionaryOfGameObjects.Add(eDiccionaryTypeObject.TypeObjectTramp, initTrampsObjects());
+        DictionaryOfGameObjects.Add(eDiccionaryTypeObject.TypeObjectMiscelaneo, initMicelaneosObjects());
+
+        //Reorganiza probabilidades
+
+        for (eDiccionaryTypeObject index = eDiccionaryTypeObject.TypeObjectStart; index < eDiccionaryTypeObject.TypeObjectEnd; index++) 
+        {
+            List<StructDataObject> bufList = new List<StructDataObject>();
+            DictionaryOfGameObjects.TryGetValue(index, out bufList);
+            for (int index2 = 1; index2 < bufList.Count; index2++) 
+            {
+                StructDataObject buf = new StructDataObject();
+                buf = bufList[index2];
+                buf.ProbabilityMinor = bufList[index2 - 1].ProbabilityMayor + 1;
+                buf.ProbabilityMayor += bufList[index2 - 1].ProbabilityMayor;
+                bufList[index2] = buf;
+            }
+        }
+    }
+
+    private void initProbablyEscenarios() 
+    {
+        ProbablyEscenarios = new Dictionary<eTypeEscenarios, StructEscenarioData>();
+        ProbablyEscenarios.Add(eTypeEscenarios.TypeNormal, new StructEscenarioData() { ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadEscenarioNormal });
+        ProbablyEscenarios.Add(eTypeEscenarios.TypeCongelado, new StructEscenarioData() { ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadEscenarioCongelado });
+        ProbablyEscenarios.Add(eTypeEscenarios.TypeFalso, new StructEscenarioData() { ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadEscenarioFalso });
+        ProbablyEscenarios.Add(eTypeEscenarios.TypeNubes, new StructEscenarioData() { ProbabilityMinor = 0, ProbabilityMayor = ProbabilidadEscenarioDeNubes });
+
+        for (eTypeEscenarios index = eTypeEscenarios.TypeEscenariosStart + 1; index < eTypeEscenarios.TypeEscenariosEnd; index++) 
+        {
+            StructEscenarioData valueObjective = new StructEscenarioData();
+            StructEscenarioData valueBefore = new StructEscenarioData();
+            ProbablyEscenarios.TryGetValue(index, out valueObjective);
+            ProbablyEscenarios.TryGetValue(index - 1, out valueBefore);
+            valueObjective.ProbabilityMinor = valueBefore.ProbabilityMayor + 1;
+            valueObjective.ProbabilityMayor += valueBefore.ProbabilityMayor;
+            ProbablyEscenarios[index] = valueObjective;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MapGenerator/sc_tile_spawner.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Hacer aparecer tiles bien chidas 
+
+public class sc_tile_spawner : MonoBehaviour
+{
+
+    public float TileSpeed;
+    public int NivelDeDificultad;
+    /*Variables de entrada al script modificables desde Unity, las probabilidades totales deben sumar 100*/
+    public int ProbablySpawnTileNormal;
+    public int ProbablySpawnTileRock;
+    public int ProbablySpawnTileFreeze;
+    public int ProbablySpawnTileFake;
+    public int ProbablySpawnTileCloud;
+    /*Variables de probabilidad de aparecer trampas sobre tiles*/
+    public int ProbablySpawnSpikes;
+    public int ProbablySpawnExplosion;
+    /*Variable de probabilidad de aparecer micelanios sobre tiles*/
+    public int ProbablySpawnMicelanios;
+
+    /*Prefabs que se utilizaran*/
+    public GameObject tileNormal;
+    public GameObject tileRock;
+    public GameObject tileFreeze;
+    public GameObject tileFake;
+    public GameObject tileCloud;
+    public GameObject objectSpikes;
+    public GameObject objectExplosion;
+    public GameObject objectMicelanio;
+
+    //Constantes
+    private const int SIZE_MATRIX_X = 10;
+    private const int SIZE_MATRIX_Y = 18;
+    private const float POSITION_MIN_X = -8.5f;
+    private const float POSITION_MIN_Y = 20.5f;
+    private const float SCREEN_STEP = 1f;
+
+    //Indice de tipo de prefabs para diccionario
+    private enum TypeObject
+    {
+        MinTypeTile,
+        Normal = MinTypeTile,
+        Rock,
+        Freeze,
+        Fake,
+        Cloud,
+        MaxTypeTile,
+        MinTypeTramp = MaxTypeTile,
+        Spikes = MinTypeTramp,
+        Explosion,
+        MaxTypeTramp,
+        MinMicelaneos = MaxTypeTramp,
+        Miscelaneos = MinMicelaneos,
+        MaxMiscelaneos
+    }
+
+    //Estructura de datos para diccionario
+    private struct TypeObjectStruct
+    {
+        public GameObject gameObject;
+        public int probablySpawnerMininum;
+        public int probablySpawnerMaximum;
+    }
+
+    //Lista de prefabs instanciados en el juego
+    private List<GameObject> l_tile;
+
+    //Diccionario de prefabs para usar en el juego
+    private Dictionary<TypeObject, TypeObjectStruct> dictionaryObjectsProbably;
+
+    //Calcula nuevas coordenadas
+    private Vector3 CalculateCoordenatesForSpawn(int x, int y) 
+    {
+        float newX = POSITION_MIN_X + (float)x;
+        float newY = POSITION_MIN_Y + (float)y;
+        return new Vector3(newX, newY, 0);
+    }
+    
+    //Metodo para agregar un prefab con su numero de index y la probabilidad de aparicion deseada en el juego
+    private void AddObjectsToDictionary(TypeObject type, GameObject Object, int probably)
+    {
+        TypeObjectStruct buf = new TypeObjectStruct();
+
+        buf.gameObject = Object;
+        buf.probablySpawnerMininum = 1;
+        buf.probablySpawnerMaximum = probably;
+
+        dictionaryObjectsProbably.Add(type, buf);
+    }
+
+    //Metodo para inicializar el diccionario de prefabs para su uso
+    private void InitializeDictionaryObjectsProbably()
+    {
+        //Inicia el diccionario
+        dictionaryObjectsProbably = new Dictionary<TypeObject, TypeObjectStruct>();
+        //Agrega cada prefab al diccionario con su index y la probabilidad insertada para las tiles
+        AddObjectsToDictionary(TypeObject.Normal, tileNormal, ProbablySpawnTileNormal);
+        AddObjectsToDictionary(TypeObject.Rock, tileRock, ProbablySpawnTileRock);
+        AddObjectsToDictionary(TypeObject.Freeze, tileFreeze, ProbablySpawnTileFreeze);
+        AddObjectsToDictionary(TypeObject.Fake, tileFake, ProbablySpawnTileFake);
+        AddObjectsToDictionary(TypeObject.Cloud, tileCloud, ProbablySpawnTileCloud);
+        //Agrega cada prefab de trampas
+        AddObjectsToDictionary(TypeObject.Spikes, objectSpikes, ProbablySpawnSpikes);
+        AddObjectsToDictionary(TypeObject.Explosion, objectExplosion, ProbablySpawnExplosion);
+        //Agrega Micelanios
+        AddObjectsToDictionary(TypeObject.Miscelaneos, objectMicelanio, ProbablySpawnMicelanios);
+
+        //Recalcula las probabilidades de manera que cada prefab tenga rangos de probabilidad y no solo porcentajes de probabilidad para Tiles
+        TypeObjectStruct value1 = new TypeObjectStruct();
+        TypeObjectStruct value2 = new TypeObjectStruct();
+
+        for (TypeObject index = TypeObject.MinTypeTile + 1; index < TypeObject.MaxTypeTile; index++)
+        {
+
+            dictionaryObjectsProbably.TryGetValue(index - 1, out value1);
+            dictionaryObjectsProbably.TryGetValue(index, out value2);
+            value2.probablySpawnerMaximum += value1.probablySpawnerMaximum;
+            value2.probablySpawnerMininum = value1.probablySpawnerMaximum + 1;
+            dictionaryObjectsProbably[index] = value2;
+        }
+
+
+    }
+
+    //Instancia un prefab usando el diccionario de prefabs, el tipo de prefab depende de num calculado de forma random
+    public void InsertObject(int x, int y)
+    {
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Starts Objects
+        l_tile = new List<GameObject>();
+        InitializeDictionaryObjectsProbably();
+
+        //Populate screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mueve cada tile a nueva posicion y elimina la que salga de rango
+        
+    }
+}
