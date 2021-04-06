3a25155b346a0186d6158ef7b7e6535d9621e677
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-04-06 16:42:47 -0500

    Merge y avances de constructor

---
+++ Assets/Scripts/MapGenerator/sc_MapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_MapGenerator : MonoBehaviour
+{
+    // Lista con objetos para cada linea del mapa
+    private List<sc_LineConstructor> _mapMatrix;
+    //Tipo de linea actual del mapa, pueden ser low, mid, high, en ese orden y se alternan
+    private enum _lineType{
+        low,
+        mid,
+        high,
+    };
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
