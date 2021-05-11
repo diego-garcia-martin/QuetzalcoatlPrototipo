51a475d8f43845e2817e09fa9182353ef24ec68f
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-05-10 21:13:04 -0500

    Vida del jugador y GameManager
    Faltan las pantallas con el mismo GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum _gameStates{
+        START,
+        PLAY,
+        GAMEOVER
+    };
+    public _gameStates GameState;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GameState = _gameStates.START;
     }
 
     // Update is called once per frame
     void Update()
     { 
-    
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player.GetComponent<sc_player>().health <= 0)
+        {
+            GameState = _gameStates.GAMEOVER;
+            StateChange();
+        }
+        
+    }
+
+    void StateChange()
+    {
+        switch(GameState)
+        {
+            case _gameStates.GAMEOVER:
+                break;
+            case _gameStates.START:
+                break;
+            case _gameStates.PLAY:
+                break;
+        }
     }
 }
