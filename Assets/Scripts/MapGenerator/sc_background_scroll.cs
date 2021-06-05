f0265c36bb22f9b2965ec2c8272737e7229f3794
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-06-04 20:59:12 -0500

    Arreglo fondo y soundtrack
    Ya suenaaaaaaa

--- Assets/Scripts/MapGenerator/sc_background_scroll.cs
+++ Assets/Scripts/MapGenerator/sc_background_scroll.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class sc_background_scroll : MonoBehaviour
 {
-    public GameObject bg1;
+    public GameObject transitionObj;
     public GameObject bg2;
     public GameObject bg3;
+    public bool transition = false;
     public float scrollSpeed = 1;
     private List<GameObject> bgs;
     // Start is called before the first frame update
     void Start()
     {
         bgs = new List<GameObject>();
-        bgs.Add(bg1);
         bgs.Add(bg2);
         bgs.Add(bg3);
+        transition = false;
+        transitionObj.transform.position = new Vector3(0, 0, 0);
+        bg2.transform.position = new Vector3(0, 12, 0);
+        bg3.transform.position = new Vector3(0, 24, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transition)
+        {
+            transitionObj.transform.position = new Vector3(0, 0, 0);
+            bg2.transform.position = new Vector3(0, 12, 0);
+            bg3.transform.position = new Vector3(0, 24, 0);
+        }
+        if (transitionObj.transform.position.y > -12)
+        {
+            transitionObj.transform.position = new Vector3(0, transitionObj.transform.position.y - (scrollSpeed * Time.deltaTime), 0);
+        }
+        else
+        {
+            transition = false;
+        }
+
         foreach (GameObject obj in bgs)
         {
             obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y - (scrollSpeed * Time.deltaTime), 0);
@@ -28,6 +47,6 @@ public class sc_background_scroll : MonoBehaviour
             {
                 obj.transform.position = new Vector3(obj.transform.position.x, 12, 0);
             }
-        }
+        }  
     }
 }
