64acade51d5dbc19d78eda14161878e4610586db
diego-garcia-martin <garcia.martin.diego@outlook.com>
2021-06-24 21:55:17 -0500

    Nuevos personahes

---
+++ Assets/Scripts/Enemys/sc_DarkAguilucho.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_DarkAguilucho : MonoBehaviour
+{
+    //Variables de componentes que tienen que ver con el movimiento
+    private Rigidbody2D r2d;
+    private Transform tr;
+    private Transform target;
+    public float flightSpeed;
+    public float attackSpeed;
+    public float lifetime;
+    private float cooldown;
+    //Variables relacionadas con las animaciones del personaje
+    private Animator animator;
+    private string currentAnim;
+    private const string IDLE = "Anim_DarkAguilucho_idle";
+    private const string ATTACK = "Anim_DarkAguilucho_attack";
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAnim = "";
+        animator = GetComponent<Animator>();
+        r2d = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+        changeAnimation(IDLE);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifetime > 0)
+        {
+            normalBehavior();
+        }
+        else
+        {
+            tr.position = Vector3.MoveTowards(tr.position, new Vector3(0, -20, 0), (flightSpeed * Time.deltaTime));
+        }
+        lifetime = lifetime - Time.deltaTime;
+    }
+
+    private void normalBehavior()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        if (target.position.x < tr.position.x) tr.localScale = new Vector3(-1, 1, 1);
+        else if (target.position.x > tr.position.x) tr.localScale = new Vector3(1, 1, 1);
+
+
+        if (cooldown > 0)
+        {
+            cooldown = cooldown - Time.deltaTime;
+        }
+
+        //Decidimos si vamos a atacar
+        if (Vector3.Distance(transform.position, target.position) < 3f && tr.position.y > target.position.y && cooldown <= 0)
+        {
+            if (Random.Range(0, 10) < 1)
+            {
+                changeAnimation(ATTACK);
+                cooldown = 3f;
+            }
+        }
+        if (currentAnim == IDLE)
+        {
+            idleFollow();
+        }
+        else if (currentAnim == ATTACK)
+        {
+            attack();
+        }
+        //Debug.Log("Target: " + target.position);
+    }
+
+    private void idleFollow()
+    {
+        changeAnimation(IDLE);
+        //Para seguir al player en el eje de las y
+
+        if (tr.position.y > target.position.y + 4f)
+        {
+            r2d.velocity = new Vector3(r2d.velocity.x, -2, 0);
+        }
+        else if (tr.position.y < target.position.y + 2f)
+        {
+            r2d.velocity = new Vector3(r2d.velocity.x, 3, 0);
+        }
+        // Para seguir al player en el eje de las x
+        if (target.position.x > tr.position.x + 2)
+        {
+            r2d.velocity = new Vector3(flightSpeed, r2d.velocity.y, 0);
+        }
+        else if (target.position.x < tr.position.x - 2)
+        {
+            r2d.velocity = new Vector3(-flightSpeed, r2d.velocity.y, 0);
+        }
+        else
+        {
+            r2d.velocity = new Vector3(0, r2d.velocity.y, 0);
+        }
+
+    }
+
+    private void attack()
+    {
+        changeAnimation(ATTACK);
+        tr.position = Vector3.MoveTowards(tr.position, target.position, (attackSpeed * Time.deltaTime));
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        changeAnimation(IDLE);
+        if (collision.gameObject.tag == "Player")
+        {
+            lifetime = 0;
+        }
+    }
+
+    private void changeAnimation(string newAnim)
+    {
+        if (currentAnim == newAnim) return;
+        animator.Play(newAnim);
+        currentAnim = newAnim;
+    }
+}
---
+++ Assets/Scripts/Enemys/sc_spacePichoncin.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_spacePichoncin : MonoBehaviour
+{
+    //Variables para los otros componentes
+    private Animator animator;
+    private Rigidbody2D r2d;
+    private Transform tr;
+    private Camera cam;
+    private Vector3 limitsMin;
+    private Vector3 limitsMax;
+    //Variables de control para ver que le pasa al pichoncin
+    private bool isGrounded;
+    private bool isFlying;
+    public int segundosAccion;
+    //Las variables que tienen que ver con las animaciones
+    private string currentAnim;
+    private const string IDLE = "Anim_SpacePichoncin_idle";
+    private const string FLY = "Anim_SpacePichoncin_vuelo";
+    private const string EAT = "Anim_SpacePichoncin_picando";
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        r2d = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+        cam = Camera.main;
+        limitsMin = cam.ScreenToWorldPoint(new Vector3(0, 0, tr.position.z));
+        limitsMax = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, tr.position.z));
+        currentAnim = "";
+        changeAnimation(FLY);
+        isGrounded = false;
+        StartCoroutine("waiter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGrounded)
+        {
+            changeAnimation(FLY);
+            if (isFlying) flying();
+            if (r2d.velocity.y < -10)
+            {
+                r2d.AddForce(new Vector2(Random.Range(-300, 300), 300));
+                isFlying = true;
+                isGrounded = false;
+            }
+        }
+        if (r2d.velocity.x != 0)
+        {
+            if (r2d.velocity.x < 0) tr.localScale = new Vector3(-1, 1, 1);
+            else tr.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (tr.position.x >= limitsMax.x - 1)
+        {
+            r2d.velocity = new Vector3(-2, r2d.velocity.x, 0);
+        }
+        else if (tr.position.x <= limitsMin.x + 1)
+        {
+            r2d.velocity = new Vector3(2, r2d.velocity.x, 0);
+        }
+
+        if (r2d.velocity.y < -10 && isGrounded)
+        {
+            r2d.AddForce(new Vector2(Random.Range(-300, 300), 300));
+            isFlying = true;
+            isGrounded = false;
+        }
+        else if (r2d.velocity.y < -3)
+        {
+            isGrounded = false;
+        }
+
+    }
+
+    private void flying()
+    {
+        if (r2d.velocity.y <= 0)
+        {
+            r2d.velocity = new Vector3(r2d.velocity.x, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Ice")
+        {
+            isGrounded = true;
+            r2d.velocity = new Vector2(0, 0);
+            changeAnimation(IDLE);
+        }
+    }
+
+    private void changeAnimation(string newAnim)
+    {
+        if (currentAnim == newAnim) return;
+        animator.Play(newAnim);
+        currentAnim = newAnim;
+    }
+
+    IEnumerator waiter()
+    {
+        for (; ; )
+        {
+            if (isGrounded && !isFlying)
+            {
+                if (Random.Range(0, 10) < 5)
+                {
+                    changeAnimation(EAT);
+                }
+                else if (Random.Range(0, 10) < 6)
+                {
+                    r2d.AddForce(new Vector2(Random.Range(-300, 300), 600));
+                    isFlying = true;
+                    isGrounded = false;
+                    sc_audioManager.PlaySound("aleteoPichon");
+                }
+                else
+                {
+                    changeAnimation(IDLE);
+                }
+            }
+            else if (isFlying)
+            {
+                if (Random.Range(0, 10) < 7)
+                {
+                    r2d.AddForce(new Vector2(Random.Range(-300, 300), 0));
+                    sc_audioManager.PlaySound("aleteoPichon");
+                }
+                else
+                {
+                    isFlying = false;
+                }
+            }
+            yield return new WaitForSeconds(segundosAccion);
+        }
+    }
+}
